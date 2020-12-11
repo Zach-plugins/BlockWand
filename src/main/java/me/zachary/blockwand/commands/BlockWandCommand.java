1e41d5335fe26ddc7556912fc950dc7177bd80bd
Zach_FR <zac232003@icloud.com>
2020-12-11 10:54:02 -0500

    Added permission to command/You can give blockwand to someone else/Added arguments when you do the command

--- src/main/java/me/zachary/blockwand/commands/BlockWandCommand.java
+++ src/main/java/me/zachary/blockwand/commands/BlockWandCommand.java
@@ -2,6 +2,7 @@ package me.zachary.blockwand.commands;
 
 import me.zachary.blockwand.Blockwand;
 import me.zachary.blockwand.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -27,8 +28,19 @@ public class BlockWandCommand extends SpigotCommand {
 
     @Override
     public CommandResult onPlayerExecute(Player player, String[] strings) {
+        if(!player.hasPermission("blockwand.give")){
+            player.sendMessage(ChatUtils.color("&4You don't have permission to execute this comamnd."));
+            return CommandResult.COMPLETED;
+        }
         if(strings[0].equalsIgnoreCase("give")){
-            player.getInventory().setItemInMainHand(plugin.getBlockWand(plugin.getConfig().getString("Default wand.Block"), plugin.getConfig().getString("Default wand.Cost")));
+            if(strings.length == 2){
+                Player target = Bukkit.getPlayer(strings[1]);
+                if(target != null){
+                    target.getInventory().setItemInMainHand(plugin.getBlockWand(plugin.getConfig().getString("Default wand.Block"), plugin.getConfig().getString("Default wand.Cost")));
+                }else
+                    player.sendMessage(ChatUtils.color("Errors, this player is not found"));
+            }else
+                player.getInventory().setItemInMainHand(plugin.getBlockWand(plugin.getConfig().getString("Default wand.Block"), plugin.getConfig().getString("Default wand.Cost")));
         }
         return CommandResult.COMPLETED;
     }
@@ -36,5 +48,24 @@ public class BlockWandCommand extends SpigotCommand {
     @Override
     public CommandResult onConsoleExecute(SpigotConsole spigotConsole, String[] strings) {
         return CommandResult.COMPLETED;
+    }
+
+    @Override
+    public List<String> getCommandComplete(Player player, String alias, String[] args) {
+        List<String> args1 = new ArrayList<>();
+        List<String> args2 = new ArrayList<>();
+        Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
+        Bukkit.getServer().getOnlinePlayers().toArray(players);
+        if(args[0].equals("give")){
+            for (int i = 0; i < players.length; i++) {
+                args2.add(players[i].getName());
+            }
+            return args2;
+        }
+        if(args[0] != null){
+            if(player.hasPermission("blockwand.give")) args1.add("give");
+            return args1;
+        }
+        return args2;
     }
 }
